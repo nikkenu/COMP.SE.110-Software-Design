0b2ea34a2794a2b2c924ac33ab009c53effc0973
Niklas Nurminen <niklas.nurminen@tuni.com>
2021-03-21 15:48:03 +0200

    Fix to datahandler

--- datahandler.cpp
+++ datahandler.cpp
@@ -19,51 +19,36 @@ void DataHandler::printData()
     for (const auto& i : values)
     {
         //if (cnt > 15) break;
-        cnt++;
         qDebug() << "Start time: " << i.start_time;
         qDebug() << "End time:   " << i.end_time;
         qDebug() << "Value:      " << i.value;
+        cnt++;
     }
 }
 
 void DataHandler::printXml(const QByteArray &data)
 {
-
-    QString alku = QString(data);
-    //qDebug() << alku;
-
     QDomDocument doc;
     doc.setContent(data);
 
     QDomElement docElem = doc.documentElement();
 
     QDomNode n = docElem.firstChild();
-    //qDebug() << n.nodeValue();
-
-    QString value;
-    QString start_time;
-    QString end_time;
-    while (!n.isNull()) {
-        QDomElement e = n.toElement();
-        if (!e.isNull()) {
-
-
+    while(!n.isNull()) {
+        QDomElement e = n.toElement(); // try to convert the node to an element.
+        if(!e.isNull()) {
             QDomElement child = e.firstChild().toElement();
-            while (!child.isNull())
-            {
-                if (child.tagName() == "value") value=child.text();
-                if (child.tagName() == "start_time") start_time=child.text();
-                if (child.tagName() == "end_time") end_time=child.text();
-
-                time_series_element tmp = {.start_time=start_time, .end_time=end_time, .value=value};
-                values.push_back(tmp);
+            time_series_element tmp;
+            while (!child.isNull()) {
+                if (child.tagName() == "value") tmp.value = child.text();
+                if (child.tagName() == "start_time") tmp.start_time = child.text();
+                if (child.tagName() == "end_time") tmp.end_time = child.text();
                 child = child.nextSibling().toElement();
             }
+            values.push_back(tmp);
         }
         n = n.nextSibling();
     }
 
-
     printData();
-
 }
